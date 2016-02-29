6513b5dd4dcfbad028c4eeb393b5008d2c8b2b09
guillaumee <guillaume.sarnette@insa-lyon.fr>
2016-02-29 11:47:17 +0100

    Hpp Expression

--- Includes/ExpressionMultiplication.hpp
+++ Includes/ExpressionMultiplication.hpp
@@ -1,1 +1,14 @@
-ExpressionMultiplication.hpp
+#ifndef EXPRESSIONMULTIPLICATION_HPP
+#define EXPRESSIONMULTIPLICATION_HPP
+
+class ExpressionMultiplication
+{
+public:
+	ExpressionMultiplication();
+	virtual ~ExpressionMultiplication();
+	
+private:
+	
+};
+
+#endif // EXPRESSIONMULTIPLICATION_HPP
--- Includes/Identifiant.hpp
+++ Includes/Identifiant.hpp
@@ -1,1 +1,14 @@
-Identifiant.hpp
+#ifndef IDENTIFIANT_HPP
+#define IDENTIFIANT_HPP
+
+class Identifiant
+{
+public:
+	Identifiant();
+	virtual ~Identifiant();
+	
+private:
+	
+};
+
+#endif // IDENTIFIANT_HPP
