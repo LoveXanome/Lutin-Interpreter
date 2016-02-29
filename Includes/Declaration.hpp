015f6268e2556f06ab78eaa0feff95642cec5e66
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-02-29 11:53:56 +0100

    Ajoute interface liste déclaration & déclaration

--- Includes/Declaration.hpp
+++ Includes/Declaration.hpp
@@ -1,1 +1,33 @@
-Declaration.hpp
+/*************************************************************************
+                           Declaration  
+*************************************************************************/
+
+#if!defined ( DECLARATION_HPP )
+#define DECLARATION_HPP
+
+#include <string>
+
+
+class Declaration
+{
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Declaration();
+    virtual ~Declaration();
+
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+	string identifiant;
+};
+
+//--------------------------- Autres définitions dépendantes de <Declaration>
+
+#endif // DECLARATION_HPP
+
--- Includes/ListeDeclaration.hpp
+++ Includes/ListeDeclaration.hpp
@@ -1,1 +1,31 @@
-ListeDeclaration.hpp
+/*************************************************************************
+                           ListeDeclaration  
+*************************************************************************/
+
+#if!defined ( LISTEDECLARATION_HPP )
+#define LISTEDECLARATION_HPP
+
+#include <string>
+
+
+class ListeDeclaration
+{
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    ListeDeclaration();
+    virtual ~ListeDeclaration();
+
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+	
+};
+
+#endif // LISTEDECLARATION_HPP
+
