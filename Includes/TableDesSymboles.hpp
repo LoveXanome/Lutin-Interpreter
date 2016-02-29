99305cb7aed424975dccf0fcf6a3f9c28022b282
David W <david.wobrock@gmail.com>
2016-02-29 11:31:21 +0100

    add forgotten table symbol files

--- Includes/TableDesSymboles.hpp
+++ Includes/TableDesSymboles.hpp
@@ -1,15 +1,15 @@
 #ifndef TABLE_DES_SYMBOLES_HPP
 #define TABLE_DES_SYMBOLES_HPP
 
-#include <map>
+#include <unordered_map>
 #include <string>
 #include "Declaration.hpp"
 
-class TableDesSymboles : public std::map<std::string, Declaration>
+class TableDesSymboles : public std::unordered_map<std::string, Declaration>
 {
-public:
-	TableDesSymboles();
-	virtual ~TableDesSymboles();
+	public:
+		TableDesSymboles();
+		virtual ~TableDesSymboles();
 };
 
 #endif // TABLE_DES_SYMBOLES_HPP
