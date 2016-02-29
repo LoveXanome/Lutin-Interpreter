574018730f00e46d499a90d15fc4b9bc1c94c81a
el comcombre <jcornevin@if501-219-07.insa-lyon.fr>
2016-02-29 11:58:43 +0100

    wip

--- Includes/ListeInsctructions.hpp
+++ Includes/ListeInsctructions.hpp
@@ -1,1 +1,24 @@
-#include "AutomateLutin.hpp"
+#ifndef LISTE_INSTRUCTIONS_HPP
+#define LISTE_INSTRUCTIONS_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Instruction.hpp"
+
+using namespace std;
+
+class ListeInstructions
+{ 
+	public:
+	 ListeInstructions();
+	 virtual ~ListeInstructions();
+	 
+	 void addInstruction(Instruction instruction);
+	 vector<Instruction>::iterator getIterator();
+	
+	private:
+		vector<Instruction> vInstructions;
+};
+
+#endif
--- Sources/ListeInsctructions.cpp
+++ Sources/ListeInsctructions.cpp
@@ -1,1 +1,21 @@
-#include "AutomateLutin.hpp"
+#include "ListeInstructions.hpp"
+
+ListeInstructions::ListeInstructions(string identifiant):identifiant(identifiant)
+{
+	
+}
+
+ListeInstructions::~ListeInstructions()
+{
+	
+}
+ 
+void ListeInstructions::addInstruction(Instruction instruction)
+{
+	this->vInstructions.push_back(instruction);
+}
+
+vector<Instruction>::iterator ListeInstructions::getIterator()
+{
+	return this->vInstructions.begin();
+}
