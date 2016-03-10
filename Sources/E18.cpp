aeddb51e350dee01cd034100fed1e6880dd2504b
Djowood <jolan.cornevin@etu.univ-lyon1.fr>
2016-03-10 22:39:53 +0100

    ajout des E13-14-18-20-22

--- Sources/E18.cpp
+++ Sources/E18.cpp
@@ -1,4 +1,8 @@
 #include "E18.hpp"
+#include "E8.hpp"
+#include "E11.hpp"
+#include "E12.hpp"
+#include "E19.hpp"
 
 E18::E18() : Etat(18)
 {
@@ -12,18 +16,26 @@ E18::~E18()
 
 valeurRetour E18::transition(AutomateLutin* automate, Symbole * s)
 {
-	switch (*s){
-        case ParentheseOuvrante :
-            automate->decalage(s, new E8);
+    valeurRetour retour;
+    switch (*s){
+        case PARENTHESE_OUVRANTE :
+            automate->decalage(s, new E8, true);
+            retour = RECONNU;
             break;
-        case Identifiant :
-            automate->decalage(s, new E11);
+        case IDENTIFIANT :
+            automate->decalage(s, new E11, true);
+            retour = RECONNU;
             break;
-        case Valeur :
-            automate->decalage(s, new E12);
+        case VALEUR :
+            automate->decalage(s, new E12, true);
+            retour = RECONNU;
             break;
         case EXP :
-            automate->decalage(s, new E17);
+            automate->decalage(s, new E19, false);
+            retour = RECONNU;
+            break;
+        default:
+            retour = NON_RECONNU;
     }
-	return NON_RECONNU;
+    return retour;
 }
