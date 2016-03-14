1bf5c3d2fe2848c1a7099b736ad40cb124372d61
Omadz <ophelie.delsaux@hotmail.fr>
2016-03-14 09:50:21 +0100

    Modification Analyse statique

--- Includes/AnalyseStatique.hpp
+++ Includes/AnalyseStatique.hpp
@@ -2,7 +2,10 @@
 #ifndef ANALYSESTATIQUE_HPP
 #define ANALYSESTATIQUE_HPP
 
+#include "Declaration.hpp"
+
 #include <string>
+#include <unordered_map>
 
 //structure pour analyse statique
 typedef struct etatIdentifiant
@@ -17,6 +20,7 @@ class AnalyseStatique
 	public:
 		AnalyseStatique();
 		virtual ~AnalyseStatique();
+		bool check(std::unordered_map<std::string, Declaration*> tableDesSymboles, std::unordered_map<std::string, EtatIdentifiant> tableAnalyseStatique);
 	private:
 };
 #endif // AUTOMATE_LUTIN_HPP
--- Sources/AnalyseStatique.cpp
+++ Sources/AnalyseStatique.cpp
@@ -1,8 +1,17 @@
 #include "AnalyseStatique.hpp"
 
 AnalyseStatique::AnalyseStatique() 
-{	
+{
+		
 }
+
 AnalyseStatique::~AnalyseStatique() 
-{	
+{
+	
+}
+
+bool AnalyseStatique::check(std::unordered_map<std::string, Declaration*> tableDesSymboles, std::unordered_map<std::string, EtatIdentifiant> tableAnalyseStatique)
+{
+	bool programmeValide = false;
+	return programmeValide;
 }
